5243d62e066a6762bfe480c050efe074d05ee9b3
Natã Bento <73320913+natabsa@users.noreply.github.com>
2024-12-25 15:01:33 +0000

    modified:   src/controller/delete/activity.js

--- src/controller/delete/activity.js
+++ src/controller/delete/activity.js
@@ -1,18 +1,14 @@
-import Activity from '../../model/activity.js';
+import Activity from "../../model/activity.js";
 
 // File for function controlling delete operations to the database
 
-async function delAtv(req, res)
-{
-    try
-    {
-       res.status(200).json(await Activity.findByIdAndDelete(req.params.id));
-    }
-    catch(error)
-    {
+export async function delAtv(req, res) {
+    try {
+        res.status(200).json(await Activity.findByIdAndDelete(req.params.id));
+    } catch (error) {
         console.error(error.toString());
-        res.status(500).json({ Error: "Fail trying to delete activity document"});
+        res.status(500).json({
+            Error: "Fail trying to delete activity document",
+        });
     }
 }
-
-export default delAtv;
